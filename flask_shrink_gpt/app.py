977f2407d87610199dd0e57b6417970937df9c1c
orizem <ori.zemach@gmail.com>
2023-11-16 13:47:00 +0200

    docker

--- flask_shrink_gpt/app.py
+++ flask_shrink_gpt/app.py
@@ -86,5 +86,5 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(host="0.0.0.0", port=3000, debug=True)
+    # app.run()
+    app.run(host="127.0.0.1", port=5000, debug=True)
