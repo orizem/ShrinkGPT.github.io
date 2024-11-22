a2fe4ce6ea9bf1af49d4228ed421b8c928dd576b
orizem <ori.zemach@gmail.com>
2024-11-22 16:33:22 +0200

    uploaded sqlite to the cloud

--- config.py
+++ config.py
@@ -5,9 +5,6 @@ import os
 SECRET_KEY = b"x*n\x9f\xa8\x00\x1b\xd4\xf4G\xaaO\x99\xafSC\x97\xcfC\x9a\x1f!\x1ai!3\xad\x06\xf5\xd2O2"
 PROJECT_PATH = f"{os.getcwd()}".replace("\\docs", "").replace("\\", "/")
 WEBSITE_PATH = f"{PROJECT_PATH}/website"
-SQLALCHEMY_DATABASE_URI = os.environ.get(
-    "DATABASE_URL", f"sqlite:///{WEBSITE_PATH}\\db.sqlite"
-)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SESSION_TYPE = "filesystem"
 SERVER_KEY_PASS = "f'A8m??`2TBE@e!"
